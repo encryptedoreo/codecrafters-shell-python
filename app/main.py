01928cfb95345a82a9144cfb04ce5b64ad6b3f31
Abhradeep De <encryptedoreo@gmail.com>
2025-02-17 20:12:14 +0000

    completed this stage

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ def main():
         sys.stdout.write("$ ")
         command = input()
 
-        sys.stdout.write(f"{command}: command not found\n")
+        match command.split()[0]:
+            case "exit": exit(int(command.split()[1]))
+            case _: sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
