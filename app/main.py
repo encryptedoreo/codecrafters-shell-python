b880687fbb6af6ef799f752d6f53cad5b8c4ebd7
Abhradeep De <encryptedoreo@gmail.com>
2025-02-17 19:55:48 +0000

    completed this stage

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ import sys
 def main():
     sys.stdout.write("$ ")
 
-    command = input()
-    sys.stdout.write(f"{command}: command not found")
+    while True:
+        command = input()
+        sys.stdout.write(f"{command}: command not found")
 
 
 if __name__ == "__main__":
