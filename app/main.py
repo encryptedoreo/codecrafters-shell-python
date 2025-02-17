d4f31f8331361dc1d8c18bc3770d727d2d217c64
Abhradeep De <encryptedoreo@gmail.com>
2025-02-17 20:16:37 +0000

    completed this stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,12 @@ def main():
         match command.split()[0]:
             case "exit": sys.exit(int(command.split()[1]))
             case "echo": sys.stdout.write(f"{' '.join(command.split()[1:])}\n")
-            case _: sys.stdout.write(f"{command}: command not found\n")
+            case "type": sys.stdout.write(
+                f"{command.split()[1]} is a shell builtin\n"
+                if command.split()[1] in {"exit", "echo", "type"}
+                else f"{command.split()[1]}: command not found\n"
+            )
+            case _: sys.stdout.write(f"{command.split()[0]}: command not found\n")
 
 
 if __name__ == "__main__":
