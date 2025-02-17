ccb3c71334c3d0b3d9d340689d9f0ac2e63dd82c
Abhradeep De <encryptedoreo@gmail.com>
2025-02-17 20:24:37 +0000

    completed this stage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import shutil
 
 
 def main():
@@ -9,9 +10,11 @@ def main():
         match command.split():
             case ["exit", arg]: sys.exit(int(arg))
             case ["echo", *args]: sys.stdout.write(f"{' '.join(args)}\n")
+
             case ["type", arg] if arg in {"type", "exit", "echo"}: sys.stdout.write(f"{arg} is a shell builtin\n")
+            case ["type", arg] if path := shutil.which(arg): sys.stdout.write(f"{arg} is {path}\n")
             case ["type", arg] if arg not in {"type", "exit", "echo"}: sys.stdout.write(f"{arg}: not found\n")
-            
+
             case _: sys.stdout.write(f"{command.split()[0]}: command not found\n")
 
 
