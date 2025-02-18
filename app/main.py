261f73b83a836ae6a3ccd5bf76090ca4a0f0e2f1
Abhradeep De <encryptedoreo@gmail.com>
2025-02-18 16:40:09 +0000

    completed this stage

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 import sys
 import shutil
 import os
+import subprocess
 import shlex
-import contextlib
 
 
 def main():
@@ -27,7 +27,7 @@ def main():
             case ["cd", dir_path] if (path_exists := os.path.exists(expanded := os.path.expanduser(dir_path))): os.chdir(expanded)
             case ["cd", dir_path] if not path_exists: stdout.write(f"cd: {os.path.expanduser(dir_path)}: No such file or directory\n")
 
-            case [fn, *args] if shutil.which(fn): os.system(command)
+            case [fn, *args] if shutil.which(fn): subprocess.run(command, shell=True, text=True)
             case [fn, *args] if not shutil.which(fn): stdout.write(f"{fn}: command not found\n")
         
         if ">" in command:
