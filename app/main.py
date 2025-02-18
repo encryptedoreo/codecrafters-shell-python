de4a89f9f9e263377d0ac592817abe67fd3833e0
Abhradeep De <encryptedoreo@gmail.com>
2025-02-18 09:54:19 +0000

    completed this stage

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def main():
             case ["exit", arg]: sys.exit(int(arg))
             case ["echo", *args]: sys.stdout.write(f"{' '.join(args)}\n")
 
-            case ["type", arg] if arg in {"type", "exit", "echo"}: sys.stdout.write(f"{arg} is a shell builtin\n")
+            case ["type", arg] if arg in {"type", "exit", "echo", "pwd"}: sys.stdout.write(f"{arg} is a shell builtin\n")
             case ["type", arg] if path := shutil.which(arg): sys.stdout.write(f"{arg} is {path}\n")
             case ["type", arg] if arg not in {"type", "exit", "echo"}: sys.stdout.write(f"{arg}: not found\n")
 
