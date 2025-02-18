05762b02d54a57dd999d61e5a542f320013720fd
Abhradeep De <encryptedoreo@gmail.com>
2025-02-18 11:21:41 +0000

    completed this stage

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ def main():
             case ["cd", dir_path] if (path_exists := os.path.exists(expanded := os.path.expanduser(dir_path))): os.chdir(expanded)
             case ["cd", dir_path] if not path_exists: sys.stdout.write(f"cd: {os.path.expanduser(dir_path)}: No such file or directory\n")
 
-            case [fn, *args] if shutil.which(fn.strip('\'"')): sys.stdout.write(subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode())
+            case [fn, *args] if shutil.which(fn.strip('\'"')): subprocess.run(cmd, stdout=sys.stdout)
             case [fn, *args] if not shutil.which(fn.strip('\'"')): sys.stdout.write(f"{fn}: command not found\n")
 
 
