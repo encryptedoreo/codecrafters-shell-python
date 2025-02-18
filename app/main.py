97f3478dc7428fb879538379eabcc0b70c7b10d0
Abhradeep De <encryptedoreo@gmail.com>
2025-02-18 12:16:34 +0000

    completed this stage

--- app/main.py
+++ app/main.py
@@ -10,15 +10,15 @@ def main():
 
     while True:
         sys.stdout.write("$ ")
-        command = sys.stdin.readline().strip().replace('>', '1>')
+        command = sys.stdin.readline().strip().replace('1>', '>')
 
         cmd = shlex.split(command, posix=True)
-        stdout = sys.stdout if ">" not in command else open(cmd[-1], 'a')
+        stdout = sys.stdout if ">" not in command else open(cmd[-1], 'w+')
 
         match cmd:
             case ["exit", arg]: sys.exit(int(arg))
             case ["exit"]: sys.exit(0)
-            case ["echo", *args]: stdout.write(f"{' '.join(args)}\n")
+            case ["echo", *args]: stdout.write(f"{' '.join(args if stdout == sys.stdout else args[:-2])}\n")
             case ["type", arg] if arg in builtins: stdout.write(f"{arg} is a shell builtin\n")
             case ["type", arg] if path := shutil.which(arg): stdout.write(f"{arg} is {path}\n")
             case ["type", arg] if arg not in builtins: stdout.write(f"{arg}: not found\n")
@@ -27,7 +27,7 @@ def main():
             case ["cd", dir_path] if (path_exists := os.path.exists(expanded := os.path.expanduser(dir_path))): os.chdir(expanded)
             case ["cd", dir_path] if not path_exists: stdout.write(f"cd: {os.path.expanduser(dir_path)}: No such file or directory\n")
 
-            case [fn, *args] if shutil.which(fn): os.system(command)
+            case [fn, *args] if shutil.which(fn): stdout.write(subprocess.run(cmd, stdout=subprocess.PIPE, text=True).stdout.rstrip())
             case [fn, *args] if not shutil.which(fn): stdout.write(f"{fn}: command not found\n")
         
         if ">" in command:
