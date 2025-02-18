df20f5ed2eac4d3a996cc80bcfcd6ccfab6f806e
Abhradeep De <encryptedoreo@gmail.com>
2025-02-18 11:27:21 +0000

    completed this stage

--- app/main.py
+++ app/main.py
@@ -10,20 +10,23 @@ def main():
         sys.stdout.write("$ ")
         command = sys.stdin.readline().strip()
 
-        match (cmd := shlex.split(command, posix=True)):
+        cmd = shlex.split(command, posix=True)
+        stdout = sys.stdout if ">" not in command else cmd[-1]
+
+        match cmd:
             case ["exit", arg]: sys.exit(int(arg))
             case ["exit"]: sys.exit(0)
-            case ["echo", *args]: sys.stdout.write(f"{' '.join(args)}\n")
-            case ["type", arg] if arg in {"type", "exit", "echo", "pwd", "cd"}: sys.stdout.write(f"{arg} is a shell builtin\n")
-            case ["type", arg] if path := shutil.which(arg): sys.stdout.write(f"{arg} is {path}\n")
-            case ["type", arg] if arg not in {"type", "exit", "echo"}: sys.stdout.write(f"{arg}: not found\n")
+            case ["echo", *args]: stdout.write(f"{' '.join(args)}\n")
+            case ["type", arg] if arg in {"type", "exit", "echo", "pwd", "cd"}: stdout.write(f"{arg} is a shell builtin\n")
+            case ["type", arg] if path := shutil.which(arg): stdout.write(f"{arg} is {path}\n")
+            case ["type", arg] if arg not in {"type", "exit", "echo"}: stdout.write(f"{arg}: not found\n")
 
             case ["pwd"]: sys.stdout.write(f"{os.getcwd()}\n")
             case ["cd", dir_path] if (path_exists := os.path.exists(expanded := os.path.expanduser(dir_path))): os.chdir(expanded)
-            case ["cd", dir_path] if not path_exists: sys.stdout.write(f"cd: {os.path.expanduser(dir_path)}: No such file or directory\n")
+            case ["cd", dir_path] if not path_exists: stdout.write(f"cd: {os.path.expanduser(dir_path)}: No such file or directory\n")
 
-            case [fn, *args] if shutil.which(fn): subprocess.run(cmd, stdout=sys.stdout)
-            case [fn, *args] if not shutil.which(fn): sys.stdout.write(f"{fn}: command not found\n")
+            case [fn, *args] if shutil.which(fn): subprocess.run(cmd, stdout=stdout)
+            case [fn, *args] if not shutil.which(fn): stdout.write(f"{fn}: command not found\n")
 
 
 if __name__ == "__main__":
