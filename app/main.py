ef25052f07efbfa59f827ecf4f697770302e264c
Abhradeep De <encryptedoreo@gmail.com>
2025-02-18 11:46:50 +0000

    completed this stage

--- app/main.py
+++ app/main.py
@@ -13,23 +13,25 @@ def main():
         command = sys.stdin.readline().strip().replace('1>', '>')
 
         cmd = shlex.split(command, posix=True)
-        stdout = sys.stdout if ">" not in command else open(cmd[-1], 'w')
+        stdout = sys.stdout if ">" not in command else open(cmd[-1], 'a')
+        output = ''
 
         match cmd:
             case ["exit", arg]: sys.exit(int(arg))
             case ["exit"]: sys.exit(0)
-            case ["echo", *args]: stdout.write(f"{' '.join(args)}\n")
-            case ["type", arg] if arg in builtins: stdout.write(f"{arg} is a shell builtin\n")
-            case ["type", arg] if path := shutil.which(arg): stdout.write(f"{arg} is {path}\n")
-            case ["type", arg] if arg not in builtins: stdout.write(f"{arg}: not found\n")
+            case ["echo", *args]: output += f"{' '.join(args)}\n"
+            case ["type", arg] if arg in builtins: output += f"{arg} is a shell builtin\n"
+            case ["type", arg] if path := shutil.which(arg): output += f"{arg} is {path}\n"
+            case ["type", arg] if arg not in builtins: output += f"{arg}: not found\n"
 
-            case ["pwd"]: sys.stdout.write(f"{os.getcwd()}\n")
+            case ["pwd"]: output += f"{os.getcwd()}\n"
             case ["cd", dir_path] if (path_exists := os.path.exists(expanded := os.path.expanduser(dir_path))): os.chdir(expanded)
-            case ["cd", dir_path] if not path_exists: stdout.write(f"cd: {os.path.expanduser(dir_path)}: No such file or directory\n")
+            case ["cd", dir_path] if not path_exists: output += f"cd: {os.path.expanduser(dir_path)}: No such file or directory\n"
 
-            case [fn, *args] if shutil.which(fn): stdout.write(subprocess.run(cmd, stdout=subprocess.PIPE, text=True).stdout.rstrip())
-            case [fn, *args] if not shutil.which(fn): stdout.write(f"{fn}: command not found\n")
+            case [fn, *args] if shutil.which(fn): output += subprocess.run(cmd, stdout=subprocess.PIPE, text=True).stdout.rstrip()
+            case [fn, *args] if not shutil.which(fn): output += f"{fn}: command not found\n"
         
+        stdout.write(output)
         if ">" in command:
             stdout.close()
 
